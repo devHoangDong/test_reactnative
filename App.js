1a4e438e93e6c3333444ee73b282eef7177ad0f1
devHoangDong <60665278+devHoangDong@users.noreply.github.com>
2023-03-30 22:43:50 +0700

    upppp

---
+++ App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  StyleSheet
+} from 'react-native';
+import AppRouter from './src/AppRouter';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
+
+
+
+function App() {
+
+  return (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
